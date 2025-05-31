54da974778b43319dc83b25c1f2c0db7bcee33df
YasmimSophia <rodrigues.santos.yasmim@escola.pr.gov.br>
2025-05-30 22:39:30 -0300

    aula-6

---
+++ portilolio-web/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+   "Geografia",
+    "Qual é o maior país da America do Sul?",
+    "Brasil"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual o nome do continente onde está localizado o Egito?",
+    "África"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual é a maior ilha do mundo?",
+    "Groelândia"
+)
